32b8235e402cf3fe17ff79f78c5dfc4432a2f6fa
Miguel Paredes <mykjestic@gmail.com>
2023-06-16 16:24:32 +0200

    Update background.js

--- chrome_plugin/background.js
+++ chrome_plugin/background.js
@@ -1,12 +1,10 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       var url = new URL(details.url);
-      if (url.hostname === "chat.openai.com" || url.hostname.endsWith(".openai.com") 
-        || url.hostname === "www.codecademy.com" || url.hostname.endsWith(".codecademy.com"))
-        {
+      if (url.hostname === "chat.openai.com" || url.hostname.endsWith(".openai.com") || url.hostname === "www.codecademy.com" || url.hostname.endsWith(".codecademy.com")) {
         return;
-        }
-        return {redirectUrl: "https://www.codecademy.com/"};
+      }
+      return {redirectUrl: "https://www.codecademy.com/"};
     },
     {urls: ["<all_urls>"]},
     ["blocking"]
