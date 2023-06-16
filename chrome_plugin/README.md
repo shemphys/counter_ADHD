c3bd5570147e4b325a3a545518d79fe079112460
Miguel Paredes <mykjestic@gmail.com>
2023-06-16 17:16:40 +0200

    .

--- chrome_plugin/README.md
+++ chrome_plugin/README.md
@@ -1,3 +1,6 @@
 # URL Control
  This might help me avoid web surfing when I should not.
  Instead, it will redirect me into my workspaces.
+
+## Future implementations
+ - Enable/Disable toolbar button
--- chrome_plugin/background.js
+++ chrome_plugin/background.js
@@ -1,12 +1,37 @@
+let allowedWebsites = ['codecademy.com', 'openai.com'];
+let allowedRequests = {};
+
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      var url = new URL(details.url);
-      if (url.hostname === "chat.openai.com" || url.hostname.endsWith(".openai.com") || url.hostname === "www.codecademy.com" || url.hostname.endsWith(".codecademy.com")) {
+  function(details) {
+    let url = new URL(details.url);
+    let originUrl = new URL(details.originUrl || details.documentUrl || '');
+
+    for (let site of allowedWebsites) {
+      if (url.hostname.endsWith(site)) {
+        allowedRequests[details.requestId] = true;
         return;
       }
-      return {redirectUrl: "https://www.codecademy.com/"};
-    },
-    {urls: ["<all_urls>"]},
-    ["blocking"]
-  );
-  
+      if (originUrl.hostname.endsWith(site) && allowedRequests[details.requestId]) {
+        return;
+      }
+    }
+
+    return {redirectUrl: 'https://www.codecademy.com/'};
+  },
+  {urls: ['<all_urls>']},
+  ['blocking']
+);
+
+chrome.webRequest.onCompleted.addListener(
+  function(details) {
+    delete allowedRequests[details.requestId];
+  },
+  {urls: ['<all_urls>']}
+);
+
+chrome.webRequest.onErrorOccurred.addListener(
+  function(details) {
+    delete allowedRequests[details.requestId];
+  },
+  {urls: ['<all_urls>']}
+);
